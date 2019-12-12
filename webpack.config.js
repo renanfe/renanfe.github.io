0d1bbcd44c3836301e4ec4dba56a8a51c2d46f2f
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2019-12-12 15:01:49 -0300

    Split css from js Add mini-css-extract-plugin to make it possible

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -8,7 +9,13 @@ module.exports = {
     filename: '[name][hash].js'
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html' })
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css',
+    })
   ],
   module: {
     rules: [
@@ -16,23 +23,21 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           {
-            loader: 'style-loader',
-            options: { 
-                insert: 'head', // insert style tag inside of <head>
-                injectType: 'singletonStyleTag' // this is for wrap all your style in just one style tag
-            },
+            loader: MiniCssExtractPlugin.loader,
           },
           'css-loader',
           'sass-loader'
         ],
       },
-      {
+       {
         test: /\.css$/i,
         use: [
-          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
           'css-loader'
         ],
-      },
+      }, 
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: [ { 
